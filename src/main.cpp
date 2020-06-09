b6bbc5295f27299afa8d7cfd0a6168f3dd382630
Henry Thasler <git@thasler.org>
2020-06-09 19:41:17 +0200

    stripped down version to clear display

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,7 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 #define LED_BUILTIN (13) // LED is connected to IO13
-
-// Environment sensor includes and defines
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-#define BME280_PIN_I2C_SCL (32)
-#define BME280_PIN_I2C_SDA (33)
-#define BME280_PIN_VCC (25)
-#define BME280_ADDR (0x76)
-#define PRESSURE_MEASUREMENT_CALIBRATION (6000)
-#define SEALEVEL_PRESSURE (1013.25)
-#define SEALEVEL_PRESSURE_CALIBRATION (9.65)
-
-TwoWire I2CBME = TwoWire(0); // set up a new Wire-Instance for BME280 Environment Sensor
-Adafruit_BME280 bme;         // use I2C
-bool environmentSensorAvailable = false;
 
 // Display stuff
 #include <GxEPD.h>
@@ -28,37 +14,18 @@ GxIO_Class io(SPI, /*CS*/ 19, /*DC*/ 18, /*RST*/ 0);
 GxEPD_Class display(io, /*RST*/ 0, /*BUSY*/ 2);
 #define HAS_RED_COLOR
 
-// FreeFonts from Adafruit_GFX
-#include <Fonts/FreeMonoBold9pt7b.h>
-#include <Fonts/FreeMonoBold12pt7b.h>
-#include <Fonts/FreeMonoBold18pt7b.h>
-#include <Fonts/FreeMonoBold24pt7b.h>
-#include <Fonts/FreeSansBold18pt7b.h>
-
-// Statistics Helper-Class
-#include <statistics.h>
-Statistics tempStats;
-Statistics humStats;
-Statistics pressStats;
-
-// Global Variables
-float currentTemperatureCelsius = 0;
-float currentHumidityPercent = 0;
-float currentPressurePascal = 0;
-
 // Track initialisation
 uint32_t initStage = 0;
 
 // Flow control, basic task scheduler
 #define SCHEDULER_MAIN_LOOP_MS (10) // ms
 uint32_t counterBase = 0;
-uint32_t counter30s = 0;
 
 // run once on startup
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_BUILTIN, HIGH);
   initStage++;
 
   // Setup serial connection for debugging
@@ -67,49 +34,14 @@ void setup()
   Serial.println("[ INIT ] - Begin");
   initStage++;
 
-  // Initialize Environment Sensor
-  pinMode(BME280_PIN_VCC, OUTPUT);
-  digitalWrite(BME280_PIN_VCC, HIGH); // Power-Supply via GPIO (BME280 uses less than 1mA).
-  delay(5);                           // wait for BMW280 to power up. Takes around 2ms.
-
-  if (I2CBME.begin(BME280_PIN_I2C_SDA, BME280_PIN_I2C_SCL, 250000U)) // set I2C-Clock to 250kHz
-  {
-    initStage++;
-    if (bme.begin(BME280_ADDR, &I2CBME)) // use custom Wire-Instance to avoid interference with other libraries.
-    {
-      initStage++;
-      environmentSensorAvailable = true;
-    }
-  }
-
-  if (environmentSensorAvailable)
-  {
-    initStage++;
-    // Setup Environment Sensor
-    bme.setSampling(Adafruit_BME280::MODE_NORMAL,     /* Operating Mode. */
-                    Adafruit_BME280::SAMPLING_X2,     /* Temp. oversampling */
-                    Adafruit_BME280::SAMPLING_X16,    /* Hum. oversampling */
-                    Adafruit_BME280::SAMPLING_X16,    /* Pressure oversampling */
-                    Adafruit_BME280::FILTER_X16,      /* Filtering. */
-                    Adafruit_BME280::STANDBY_MS_500); /* Standby time. */
-  }
-  else
-  {
-    Serial.println("[ ERROR ] - Could not find a BME280 sensor, check wiring!");
-  }
-  initStage++;
-
   delay(100);
   display.init(115200);
-  display.setTextColor(GxEPD_BLACK);
-  display.setFont(&FreeMonoBold18pt7b);
-  display.fillScreen(GxEPD_WHITE);
-  // display.eraseDisplay(true);
+  display.eraseDisplay(true);
   delay(100);
 
   initStage++; // Init complete
   Serial.printf("[ INIT ] - Completed at stage %u\n", initStage);
-  digitalWrite(LED_BUILTIN, HIGH); // turn on LED to indicate normal operation;
+  digitalWrite(LED_BUILTIN, LOW); // turn on LED to indicate normal operation;
 }
 
 // run forever
@@ -118,60 +50,13 @@ void loop()
   // 100ms Tasks
   if (!(counterBase % (100 / SCHEDULER_MAIN_LOOP_MS)))
   {
-    digitalWrite(LED_BUILTIN, HIGH);
-  }
-
-  // 500ms Tasks
-  if (!(counterBase % (500 / SCHEDULER_MAIN_LOOP_MS)))
-  {
+    digitalWrite(LED_BUILTIN, LOW);
   }
 
   // 2s Tasks
   if (!(counterBase % (2000 / SCHEDULER_MAIN_LOOP_MS)))
   {
-    if (environmentSensorAvailable)
-    {
-      // indicate measurement
-      digitalWrite(LED_BUILTIN, LOW);
-
-      // read current measurements
-      currentTemperatureCelsius = bme.readTemperature();
-      currentHumidityPercent = bme.readHumidity();
-      currentPressurePascal = bme.readPressure() + PRESSURE_MEASUREMENT_CALIBRATION;
-
-      // update statistics for each measurement
-      tempStats.update(currentTemperatureCelsius);
-      humStats.update(currentHumidityPercent);
-      pressStats.update(currentPressurePascal / 100.); // use hPa
-
-      Serial.printf("Temperature = %.1f °C   Humidity = %.0f %%   Pressure = %.0f hPa   Altitude = %.0f m\n",
-                    currentTemperatureCelsius,
-                    currentHumidityPercent,
-                    currentPressurePascal / 100.,
-                    bme.readAltitude(SEALEVEL_PRESSURE + SEALEVEL_PRESSURE_CALIBRATION));
-    }
-    else
-    {
-      Serial.println("Environment sensor not available.");
-    }
-  }
-
-  // 180s Tasks
-  // e-Paper Display MUST not be updated more often than every 180s to ensure long-term function
-  if (!(counterBase % (180000L / SCHEDULER_MAIN_LOOP_MS)))
-  {
-    counter30s++;
-    int offset = (counter30s % 5) * 40;
-    display.fillScreen(GxEPD_WHITE);
-    display.fillRoundRect(0, offset, 128, 64, 10, GxEPD_BLACK);
-    display.fillRoundRect(2, 2 + offset, 124, 60, 8, GxEPD_RED);
-    display.setCursor(4, 40 + offset);
-    display.setFont(&FreeMonoBold18pt7b);
-    display.printf("%.1f°C", currentTemperatureCelsius);
-    display.setFont(&FreeSansBold18pt7b);
-    display.setCursor(4, 100 + offset);
-    display.print("Hello World!");
-    display.update();
+  digitalWrite(LED_BUILTIN, HIGH);
   }
 
   delay(SCHEDULER_MAIN_LOOP_MS);
